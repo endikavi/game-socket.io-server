a85f49e7b75e14c05b3d026e29fa5af689428072
endikavi <endika.vi@gmail.com>
2018-05-12 17:33:22 +0200

    yui

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const server = require('http').createServer();
 
 const io = require('socket.io')(server);
 
+io.on('connection', function(socket){
+    
+    console.log('a user connected');
+    
+    socket.on('chat', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat', msg);
+    });
+    
+    socket.on('disconnect', function(){
+        
+        console.log('user disconnected');
+        
+    });
+    
+});
+
 server.listen(process.env.PORT || 3000);
 
 console.log(process.env.PORT);
