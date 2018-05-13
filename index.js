bd65f2efaab3efb405439e0deb98bd8ec74bcef8
endikavi <endika.vi@gmail.com>
2018-05-13 14:29:49 +0200

    cvvcb

--- index.js
+++ index.js
@@ -2,30 +2,60 @@ const server = require('http').createServer();
 
 const io = require('socket.io')(server);
 
+
+var players = {};
+var rooms = {};
 io.on('connection', function(socket){
 	
 	socket.actualP;
     socket.playerId;
 	
     console.log('a user connected');
-    
+	
     socket.on('chat', function(msg){
         console.log('message: ' + msg);
         io.emit('chat', msg);
     });
 	
 	socket.on('id', function(msg){
-        console.log('message: ' + msg);
+        console.log('Jugador identificado como: ' + msg);
 		socket.playerId = msg;
-        io.emit('id', msg);
+		players[msg] = socket.id;
+    });
+	
+	socket.on('newRoom', function(msg){
+        console.log('Creada sala: ' + msg);
+		rooms[msg] = 1;
+		io.emit('newRoom', msg);
+    });
+	
+	socket.on('enterRoom', function(msg){
+		if(rooms[msg] < 4 ){
+			console.log(socket.playerId + ' entrando en sala: ' + msg);
+			rooms[msg]+= 1;
+			io.emit('newRoom', msg);
+		}else{
+			console.log('Sala llena');
+			io.emit('RoomFull', msg);
+		}	
+    });
+	
+	socket.on('exitRoom', function(msg){
+		
+        console.log(socket.playerId + 'saliendo de la sala: ' + msg);
+		rooms[msg] -= 1;
+		io.emit('exitRoom', msg);
+		
     });
 	
     socket.on('walking', function(msg){
+		
 		if (socket.actualP != JSON.stringify(msg)){
 			console.log('walking to ' + msg);
 			socket.actualP = JSON.stringify(msg);
         	io.emit('walking', msg);
 		}
+		
     });
 	
     socket.on('disconnect', function(){
