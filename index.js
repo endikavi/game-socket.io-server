435a80167dca2fc2ab7b81f761fb7e6026c0a5f8
endikavi <endika.vi@gmail.com>
2018-05-15 17:43:18 +0200

    uyiuyi

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ io.on('connection', function(socket){
     });
 	
 	socket.on('globalChat', function(msg){
-        
-		globalChats.push(msg);
-		console.log(msg);
-        io.emit('globalChat', msg);
-        
+        if(players[socket.playerId]!=undefined){
+            globalChats.push(msg);
+            console.log(msg);
+            io.emit('globalChat', msg);
+        }
     });
 	
 	socket.on('id', function(msg){
@@ -48,64 +48,67 @@ io.on('connection', function(socket){
     });
 	
 	socket.on('newRoom', function(msg){
-        console.log('Creada sala: ' + msg[0] + 'por el usuario: ' + msg[1]);
-        rooms[msg[0]] = {};
-		rooms[msg[0]].people = [{1 : msg[1]}];
-        rooms[msg[0]].chief = msg[1];
-		socket.join("/"+msg[0])
-		players[socket.playerId].room = msg[0];
-		io.emit('newRoom', rooms);
+        if(players[socket.playerId]!=undefined){
+            console.log('Creada sala: ' + msg[0] + 'por el usuario: ' + msg[1]);
+            rooms[msg[0]] = {};
+            rooms[msg[0]].people = [{1 : msg[1]}];
+            rooms[msg[0]].chief = msg[1];
+            socket.join("/"+msg[0])
+            players[socket.playerId].room = msg[0];
+            io.emit('newRoom', rooms);
+        }
     });
 	
 	socket.on('enterRoom', function(msg){
-		if(rooms[msg] < 4 ){
-			console.log(socket.playerId + ' entrando en sala: ' + msg);
-			rooms[msg]+= 1;
-			socket.join("/"+msg);
-			players[socket.playerId].room = msg;
-			io.to("/"+msg).emit('enterRoom', socket.playerId);
-			io.emit('newRoom', rooms);
-		}else{
-			console.log('Sala llena');
-			io.to(socket.id).emit('enterRoom', false);
-            io.to(players[socket.playerId].room).emit('allRoomChats', msg);
-			io.emit('newRoom', rooms);
-		}	
+        if(players[socket.playerId]!=undefined){
+            if(rooms[msg] < 4 ){
+                console.log(socket.playerId + ' entrando en sala: ' + msg);
+                rooms[msg]+= 1;
+                socket.join("/"+msg);
+                players[socket.playerId].room = msg;
+                io.to("/"+msg).emit('enterRoom', socket.playerId);
+                io.emit('newRoom', rooms);
+            }else{
+                console.log('Sala llena');
+                io.to(socket.id).emit('enterRoom', false);
+                io.to(players[socket.playerId].room).emit('allRoomChats', msg);
+                io.emit('newRoom', rooms);
+            }	
+        }
     });
 	
 	socket.on('exitRoom', function(){
-        
-		socket.leave("/"+players[socket.playerId].room);
-        exitRoom(socket);
-		io.emit('newRoom', rooms);
-		
-    });
-	
-    socket.on('walking', function(msg){
-		
-		if (socket.actualP != JSON.stringify(msg)){
-			socket.actualP = JSON.stringify(msg);
-        	io.emit('walking', socket.actualP);
-		}
-		
-    });
-	
-    socket.on('disconnect', function(){
-        
-        if(players[socket.playerId].room != undefined){
-            
+        if(players[socket.playerId]!=undefined){
             socket.leave("/"+players[socket.playerId].room);
             exitRoom(socket);
             io.emit('newRoom', rooms);
-            
         }
-        
-        players[socket.playerId].online = false;
-        console.log(socket.playerId + ' desconectado');
-        io.emit('playersList', players);
-        
     });
-    
+	
+    socket.on('walking', function(msg){
+		if(players[socket.playerId]!=undefined){
+            if (socket.actualP != JSON.stringify(msg)){
+                socket.actualP = JSON.stringify(msg);
+                io.emit('walking', socket.actualP);
+            }
+        }
+    });
+	
+    socket.on('disconnect', function(){
+        if(players[socket.playerId]!=undefined){
+            if(players[socket.playerId].room != undefined){
+
+                socket.leave("/"+players[socket.playerId].room);
+                exitRoom(socket);
+                io.emit('newRoom', rooms);
+
+            }
+
+            players[socket.playerId].online = false;
+            console.log(socket.playerId + ' desconectado');
+            io.emit('playersList', players);
+        }
+    });
 });
 
 function exitRoom(socket){
